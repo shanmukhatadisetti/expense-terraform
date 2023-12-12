31f99d5c0fee164e23d7ee49a89393757d404723
shanmukh <shanmukhatadisetti@gmail.com>
2023-12-12 20:26:35 +0530

    Expense Script

--- expense_version-1/main.tf
+++ expense_version-1/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "frontend" {
-  ami           = "ami-03265a0778a880afb"
+  ami           = [data.aws_ami.ami.image_id]
   instance_type = "t3.micro"
   vpc_security_group_ids = ["sg-019ae3d5fa113c712"]
 
