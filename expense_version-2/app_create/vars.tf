be07a627ae27ef039e94cf26fa9171f5ca64a8e8
shanmukh <shanmukhatadisetti@gmail.com>
2023-12-18 22:42:44 +0530

    Expense Script

--- expense_version-2/app_create/vars.tf
+++
@@ -1,5 +0,0 @@
-variable "zone_id" {
-  default = "autonagar.in"
-}
-
-variable "component" {}
--- expense_version-2/main.tf
+++
@@ -1,9 +0,0 @@
-module "expense" {
-  count = 3
-  source = "./app_create"
-  component = var.expense[count.index]
-}
-
-variable "expense" {
-  default = ["frontend","backend","mysql"]
-}
