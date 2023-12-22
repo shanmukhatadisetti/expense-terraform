3a2ac20cf3ecd1eb00ff1dc50d3351192006e26c
shanmukh <shanmukhatadisetti@gmail.com>
2023-12-22 23:58:54 +0530

    Expense Script

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -57,3 +57,17 @@ resource "aws_route_table" "public" {
     Name = "public"
   }
 }
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/16"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "public"
+  }
+}
+
