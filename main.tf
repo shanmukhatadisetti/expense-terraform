af05a437f06e428b389cc0f68dd780c0bd05c9ea
shanmukh <shanmukhatadisetti@gmail.com>
2023-12-25 18:03:36 +0530

    Expense Script

--- main.tf
+++ main.tf
@@ -44,3 +44,15 @@ module "frontend" {
   vpc_cidr                  = var.vpc_cidr
   vpc_id                    = module.vpc.vpc_id
 }
+
+module "backend" {
+  source = "./modules/application-servers"
+
+  application-server_port   = 8080
+  component                 = "backend"
+  env                       = var.env
+  instance_type             = "t3.micro"
+  subnets                   = module.vpc.private_subnets
+  vpc_cidr                  = var.vpc_cidr
+  vpc_id                    = module.vpc.vpc_id
+}
