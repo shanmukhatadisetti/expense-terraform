1f278e106e7f4f88dcf67eb4525b82ffd9484809
shanmukh <shanmukhatadisetti@gmail.com>
2023-12-26 23:41:30 +0530

    Expense Script

--- modules/application-servers/userdata.sh
+++ modules/application-servers/userdata.sh
@@ -1,1 +1,5 @@
 #!/bin/bash
+
+dnf install python3.11-pip -python3
+pip3.11 install boto3 botocore
+ansible-pull -i localhost, -U https://github.com/shanmukhatadisetti/infra-ansible.git main.yml -e role_name=${role_name}
