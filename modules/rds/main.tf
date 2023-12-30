195d6e240c45d249b780a3543d273b19954768a1
shanmukh <shanmukhatadisetti@gmail.com>
2023-12-30 16:27:23 +0530

    Expense Script

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -39,7 +39,7 @@ resource "aws_rds_cluster" "rds_cluster" {
   engine_version          = "5.7.mysql_aurora.2.11.3"
   db_subnet_group_name    = aws_db_subnet_group.subnet_group.name
   database_name           = "dummy"
-  master_username         = data.aws_ssm_parameter.master_username
-  master_password         = data.aws_ssm_parameter.master_password
+  master_username         = data.aws_ssm_parameter.master_username.value
+  master_password         = data.aws_ssm_parameter.master_password.value
   vpc_security_group_ids  = aws_security_group.security_group.id
 }
