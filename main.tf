ddaf28de0d8e19f3f7cd6cc1788bb0934d72c55f
shanmukh <shanmukhatadisetti@gmail.com>
2023-12-31 14:34:20 +0530

    Expense Script

--- main.tf
+++ main.tf
@@ -22,17 +22,17 @@ module "vpc" {
 #  vpc_id            = module.vpc.vpc_id
 #}
 #
-#module "private_alb" {
-#  source = "./modules/alb"
-#
-#  alb_sg_allow_cidr = var.vpc_cidr
-#  alb_type          = "private"
-#  env               = var.env
-#  internal          = true
-#  subnets           = module.vpc.private_subnets
-#  vpc_id            = module.vpc.vpc_id
-#}
-#
+module "private_alb" {
+  source = "./modules/alb"
+
+  alb_sg_allow_cidr = var.vpc_cidr
+  alb_type          = "private"
+  env               = var.env
+  internal          = true
+  subnets           = module.vpc.private_subnets
+  vpc_id            = module.vpc.vpc_id
+}
+
 #module "frontend" {
 #  source = "./modules/application-servers"
 #
