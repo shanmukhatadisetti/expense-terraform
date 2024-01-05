35179024b63ff767641ead997337a0a1e7dd0f58
shanmukh <shanmukhatadisetti@gmail.com>
2024-01-05 19:45:37 +0530

    Expense Script

--- main.tf
+++ main.tf
@@ -36,19 +36,19 @@ module "private_alb" {
   tg_arn            = module.backend.tg_arn
 }
 
-#module "frontend" {
-#  source = "./modules/application-servers"
-#
-#  application-server_port   = 80
-#  component                 = "frontend"
-#  env                       = var.env
-#  instance_type             = "t3.micro"
-#  subnets                   = module.vpc.private_subnets
-#  vpc_cidr                  = var.vpc_cidr
-#  vpc_id                    = module.vpc.vpc_id
-#  bastion_node_cidr         = var.bastion_node_cidr
-#}
-#
+module "frontend" {
+  source = "./modules/application-servers"
+
+  application-server_port   = 80
+  component                 = "frontend"
+  env                       = var.env
+  instance_type             = "t3.micro"
+  subnets                   = module.vpc.private_subnets
+  vpc_cidr                  = var.vpc_cidr
+  vpc_id                    = module.vpc.vpc_id
+  bastion_node_cidr         = var.bastion_node_cidr
+}
+
 module "backend" {
   source = "./modules/application-servers"
   depends_on = [module.mysql]
